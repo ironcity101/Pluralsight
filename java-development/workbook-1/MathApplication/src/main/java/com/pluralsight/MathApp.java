277abb34485570d575ef4bf8218a50dbc77aa16d
ironcity101 <tdaini@yearup.org>
2024-04-11 15:27:59 -0400

    completed mod3 excerise 2 - Math

---
+++ java-development/workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+       /*float Bobsalary = 2100000;
+        float Garysalary = 274274;
+        float highestsalary = Math.max(Garysalary, Bobsalary);
+        System.out.println("The Highest salary is " +highestsalary);*/
+
+        
+
+
+
+
+
+
+
+    }
+}
